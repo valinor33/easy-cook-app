1ec49bf260b3d647ab6ae5e9562e3b0e520f3a3f
Andres Lopez Menchaca <andresfullstack@gmail.com>
2023-03-12 20:06:23 -0300

    update - added form container

--- src/components/search-form/search-form.tsx
+++ src/components/search-form/search-form.tsx
@@ -1,6 +1,12 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { SearchInput } from "../search-input/search-input";
+
+import styles from "./search-form.css?inline"
 
 export const SearchForm = component$(() => {
+  useStylesScoped$(styles);
 
-    return <>Hola</>
+    return <div class='search-form-wrapper'>
+        <SearchInput />
+    </div>
 })
---
+++ src/components/search-input/search-input.tsx
@@ -0,0 +1,8 @@
+import { component$ } from "@builder.io/qwik";
+
+
+export const SearchInput = component$(() => {
+    return <>
+    <input type="text" name="search" id="search" />
+    </>
+})
